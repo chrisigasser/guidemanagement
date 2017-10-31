c7a10ec4d1e2e1f27ac4de0ae619c2c5fb056296
chrisigasser <christoph.gasser1@gmail.com>
2017-10-31 13:57:15 +0100

    added file

--- NodeAPI/index.js
+++ NodeAPI/index.js
@@ -5,7 +5,7 @@ var url = "mongodb://localhost:27017/mydb";
 var app = express();
  
 app.get('/nextStation', function(req, res) {
-  res.json({"Name": "Currently working on this feature"});
+  res.json({Name: "Currently working on this feature"});
 });
 
 app.get('/stations', function(req, res) {
