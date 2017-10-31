916debfe17cc536167b7f09a654d5719f4aa3cf6
chrisigasser <christoph.gasser1@gmail.com>
2017-10-31 14:05:48 +0100

    changed

--- NodeAPI/index.js
+++ NodeAPI/index.js
@@ -1,15 +1,30 @@
-var express = require('express')
-var MongoClient = require('mongodb').MongoClient;
-
-var url = "mongodb://localhost:27017/mydb";
+var express = require('express');
 var app = express();
- 
-app.get('/nextStation', function(req, res) {
-  res.json({Name: "Currently working on this feature"});
-});
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/guidemanagement";
+var SHA256 = require("crypto-js/sha256");
 
-app.get('/stations', function(req, res) {
+ 
+
+ 
+app.get('/users', function(req, res) {
+
+	var username = req.query.username;
+	var reqpwd = req.query.passwd;
+	var response = "FAILED";
+		
+	MongoClient.connect(url, function(err, db) {
+	  if (err) throw err;
+	  db.collection("users").findOne({}, {'username':username}, function(err, result) {
+		if (err) throw err;
+		if(rewpwd == SHA256(result.pwd)
+			response = "PASSED";
+		
+		db.close();
+	  });
+	});
 	
-});
+	res.write(response);
+})
  
 app.listen(3000)
