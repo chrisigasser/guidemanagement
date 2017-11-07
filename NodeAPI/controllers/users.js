fc87c836dda9f33d6a944f8285518c01d12e38af
fabiantschofenig <tschofe18@gmail.com>
2017-11-07 14:51:11 +0100

    -

--- NodeAPI/controllers/users.js
+++ NodeAPI/controllers/users.js
@@ -1,7 +1,8 @@
 var mongoUri = 'mongodb://localhost:27017/guidemanagement';
 var mongoose = require('mongoose');
 mongoose.connect(mongoUri);
-User = mongoose.model('User');
+var User = require('../models/User');
+//User = mongoose.model('User');
 
 exports.authenticate = function (req, res) {
   User.find({}, function (err, results) {
