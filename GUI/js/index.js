c6a233c099032863f5e0ddbab4f2a591eeee4a85
chrisigasser <christoph.gasser1@gmail.com>
2017-11-07 15:42:50 +0100

    added CORS functionality

--- GUI/js/index.js
+++ GUI/js/index.js
@@ -1,18 +1,25 @@
 var app = angular.module('myApp', []);
+var baseURL = "http://192.168.234.133:3000";
+
 app.controller('myCtrl', function($scope, $http, $timeout) {
 	$scope.checkLogin = function() {
-		$http({
-			method : "GET",
-			url : "http://localhost/checkAuthentification.html?username=asdf&pwd=abc"
-		}).then(function mySuccess(response) {
-			if(response.data != "PASSED") {
-				$scope.auth_msg = "INVALID";
-			}
-			else {
-				alert("open next");
-			}
-		}, function myError(response) {
-			$scope.auth_msg = "Error occured during contacting server; Code: " + response.status;
-		});
+		var c = new Crypt();
+		var dataObj = {username: $scope.txt_User, pwd: c.HASH.sha256($scope.txt_pwd)}
+		
+		
+		$http.post(baseURL+"/checkAuth", dataObj)
+			.then(
+				function mySuccess(response) {
+					if(response.data != "PASSED") {
+					$scope.auth_msg = "INVALID";
+					}
+					else {
+						alert("open next");
+					}
+				},
+				function myError(response) {
+					$scope.auth_msg = "Error occured during contacting server; Code: " + response.status;
+				}
+			);
 	}
 });
