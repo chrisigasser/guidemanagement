4e346a35a60cdd9e2ce22aac7b3ec9e44cec3a53
fabiantschofenig <tschofe18@gmail.com>
2017-11-07 15:00:45 +0100

    post

--- NodeAPI/Routes.js
+++ NodeAPI/Routes.js
@@ -1,4 +1,4 @@
 module.exports = function(app){
     var users = require('./controllers/users');
-    app.get('/checkAuthentification', users.authenticate);
+    app.post('/checkAuthentification', users.authenticate);
 }
