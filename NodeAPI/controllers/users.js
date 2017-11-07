234aa924c51906b6e6add66482b5a4745a586396
fabiantschofenig <tschofe18@gmail.com>
2017-11-07 16:13:39 +0100

    -

--- NodeAPI/controllers/users.js
+++ NodeAPI/controllers/users.js
@@ -6,8 +6,8 @@ var User = require('../models/user');
 
 exports.authenticate = function (req, res) {
   User.find({}, function (err, results) {
-    //console.log(req.body.username);
-    console.log(req.username);
+    console.log(req.body);
+    //console.log(req.username);
     /*results.forEach(function(element) {
       console.log('username:' + element.username);
     }, this);*/
