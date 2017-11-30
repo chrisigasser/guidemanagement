9961344df42f33f350d17368697ce3ebba76a8e3
chrisigasser <christoph.gasser1@gmail.com>
2017-11-30 21:39:23 +0100

    added scripts and log of server running

---
+++ setup_scripts/fillDBWithTestData.ps1
@@ -0,0 +1,4 @@
+
+
+Write-Host "Press any key to finish...";
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
--- singlefileNode/index.js
+++ singlefileNode/index.js
@@ -138,3 +138,4 @@ function checkCredentials(credentials) {
 }
 
 app.listen(3000);
+console.log("Server up and running on port 3000");
