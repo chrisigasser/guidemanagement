1319ad044107b037aee485042409fdbb9c1bc864
chrisigasser <christoph.gasser1@gmail.com>
2017-12-13 21:48:40 +0100

    route simulation added

--- GUI/simulator/js/index.js
+++ GUI/simulator/js/index.js
@@ -1,18 +1,71 @@
 ﻿var app = angular.module('myApp', []);
-var baseURL = "http://localhost:3000";
-var nameOfStationWhichIsUsedForGenerateStation = "next = generated";
+var serverURL = "http://localhost:3000";
+var baseURL = "/pre/simulator";
 
-var credentialObject = null;
+var credentialObject = {credentials: {username: "guide", pwd: "6a2200b0cc85d41f7e0d6e3194dc8f04eabb3a3f6d891b7c2c8b072787c0d80c"}};
 
-var allRouteWorkers = [];
+var newRouteWorker = undefined;
 
 app.controller('myCtrl', ["$scope", "$http", "$timeout", function ($scope, $http, $timeout) {
+    $scope.ankunft = 5;
+
     $scope.startNewRouteWorker = function() {
-        var newRouteWorker = new Worker('/pre/simulator/js/newRouteWorker.js');
+        newRouteWorker = new Worker(baseURL+'/js/newRouteWorker.js');
+        sendNewAnkunftToWorker($scope.ankunft);
+        sendServerURLToWorker(serverURL);
+        sendCredentialsToWorker(credentialObject);
+        
         newRouteWorker.onmessage = function(data) {
-            console.log("Got message from newRouteWorker: " + data.data);
+            switch(data.data.type) {
+                case "newRoute":
+                    var rWorker = new Worker(baseURL+"/js/routeWorker.js");
+                    rWorker.onmessage = onMessageRouteWorker;
+                    rWorker.postMessage({type: "init", route: data.data.route});
+                    rWorker.postMessage({type: "init server", value: serverURL});
+                    rWorker.postMessage({type: "init credentials", value: credentialObject});
+                    rWorker.postMessage({type: "run"});
+                    break;
+            }
         }
-        newRouteWorker.postMessage("start");
+        newRouteWorker.postMessage({type: "start"});
+    }
+
+    function onMessageRouteWorker(data) {
+        switch(data.data.type) {
+            case "route finished":
+                console.log("Route finished successful!");
+
+                break;
+        }
+    }
+
+    $scope.setNewAnkunft = function() {
+        console.log($scope.ankunft);
+        if(newRouteWorker != undefined) {
+            sendNewAnkunftToWorker($scope.ankunft);
+        }
+    }
+
+    function sendNewAnkunftToWorker(newValue) {
+        newRouteWorker.postMessage({type: "ankunft", value: newValue});
+    }
+    function sendServerURLToWorker(url) {
+        newRouteWorker.postMessage({type: "init server", value: url});
+    }
+    function sendCredentialsToWorker(cred) {
+        newRouteWorker.postMessage({type: "init credentials", value: cred});
+    }
+
+    function postFunction(relativeURL, obj, success, error) {
+        $http.post(relativeURL, obj)
+        .then(
+            function mySuccess(response) {
+                success(response);
+            },
+            function myError(response) {
+                error(response);
+            }
+        );
     }
 }]);
 
--- GUI/simulator/js/routeWorker.js
+++ GUI/simulator/js/routeWorker.js
@@ -1,3 +1,73 @@
+var myRoute = undefined;
+var serverURL = undefined;
+var credentialObject = undefined;
+
 onmessage = function(data) {
-    console.log("starting new Route");
+    switch(data.data.type) {
+        case "init":
+            myRoute = data.data.route;
+            log("New routeworker running");
+            break;
+        case "init server":
+            serverURL=data.data.value;
+            log("Server URL set");
+            break;
+        case "init credentials":
+            credentialObject = data.data.value;
+            log("credentials arrived");
+            break;
+        case "run":
+            start();
+            log("Starting simulating to run around!");
+            break;
+    }
+}
+
+function start() {
+    for(var i = 0; i < 10000; i ++) {
+        console.log("hi there");
+    }
+    finishRoute();
+}
+
+function finishRoute() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            if(this.status == 200) {
+                var message = this.responseText;
+                if(message == "1") {
+                    postMessage({type: "route finished", routeID: myRoute.id});
+                }
+                else {
+                    console.log("******ERROR********");
+                    log("at finishRoute");
+                    log("Response: " + this.responseText);
+                    console.log(this);
+                    log("******END**********");
+                }
+            }
+            else {
+                console.log("******ERROR********");
+                log("at finishRoute");
+                log("Response: " + this.responseText);
+                console.log(this);
+                log("******END**********");
+            }
+        }
+    };
+    xhttp.open("POST", serverURL+"/endRoute", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    var obj = cloneCredentialsObject();
+    obj.id = myRoute.id;
+    obj.endtime = Math.round(new Date().getTime() / 1000);
+    xhttp.send(JSON.stringify(obj));
+}
+
+function log(message) {
+    console.log("routeWorker from route: " + myRoute.id + " :      " + message);
+}
+
+function cloneCredentialsObject() {
+    return { "credentials": { "username": credentialObject.credentials.username, "pwd": credentialObject.credentials.pwd } };
 }
