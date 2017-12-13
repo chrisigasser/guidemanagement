5e36a643d635911d35997f599f8400a220aeca2d
chrisigasser <christoph.gasser1@gmail.com>
2017-12-13 09:18:57 +0100

    added basic angular

---
+++ GUI/simulator/js/index.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module('myApp', []);
+var baseURL = "http://localhost:3000";
+var nameOfStationWhichIsUsedForGenerateStation = "next = generated";
+
+var credentialObject = null;
+
+app.controller('myCtrl', ["$scope", "$http", "$timeout", function ($scope, $http, $timeout) {
+    
+}]);
+
+function cloneCredentialsObject() {
+    return { "credentials": { "username": credentialObject.credentials.username, "pwd": credentialObject.credentials.pwd } };
+}
